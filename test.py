7d16ff45f6f27f8e76216cf6caa44bc41294a3eb
Git-Uzair <uzairahmed.x@gmail.com>
2026-05-05 13:49:36 +0300

    Removed test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import pythoncom
-import win32com.client
-
-pythoncom.CoInitialize()
-app = win32com.client.GetActiveObject("Word.Application")
-doc = app.ActiveDocument
-print(f"len(doc.Content.Text) = {len(doc.Content.Text)}")
-print(f"doc.Content.End       = {doc.Content.End}")
-print(f"doc.Content.Start     = {doc.Content.Start}")
